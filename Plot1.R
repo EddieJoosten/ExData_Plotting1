f086bda9c7afad1d6d030a7854df4829708a583a
EddieJoosten <64706428+EddieJoosten@users.noreply.github.com>
2020-06-07 10:19:01 +0200

    Comment added top plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,6 +1,20 @@
+# The data file from https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip has been unzipped
+# Set the working directory to the right folder.
+setwd("D:/Documenten/Data Science/4. Exploratory Data Analysis/exdata_data_household_power_consumption")
+# Now read the dataset 
 hhpwr<-read.csv("household_power_consumption.txt", sep = ";", na.strings = "?", header = TRUE)
+#
+# Convert the date field to a proper date
 hhpwr$Date<-as.Date(hhpwr$Date,"%d/%m/%Y")
+#
+# Select only the data from the first end second of February 2007
 hhpwr_sub<-hhpwr[hhpwr$Date=="2007-02-01"|hhpwr$Date=="2007-02-02",]
+#
+# Open the png-output file
 png(file = "Plot1.png", width = 480, height = 480)
+#
+# Create the plot.
 hist(hhpwr_sub$Global_active_power,col="red",main = "Gloabal Active Power", xlab = "Global Active Power (kilowatts)",ylab = "Frequency")
+#
+# Close the device
 dev.off()
